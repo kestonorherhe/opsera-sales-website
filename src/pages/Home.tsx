82b62bfea8776c56995fb69a2faedab1249672d2
kestonorherhe <kestonorherhe@gmail.com>
2025-07-31 12:29:39 +0100

    effected changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1277,7 +1277,127 @@ function Home() {
         </div>
       </section>
 
-      <section id="get-started" className="pt-9">
+      <section className="bg-light mt-8 mb-8">
+        <div className="container">
+          
+          <div className="row align-items-center">
+            <div className="col-lg-5 mb-5 mb-sm-0">
+              <h2 className="mb-4">Frequently Asked Questions</h2>
+              <p className="mb-4">Need a help with something? Here are our most frequently asked questions.</p>
+            </div>
+
+            <div className="col-lg-7 ps-xl-6">
+              <div className="accordion accordion-icon accordion-border" id="accordionFaq">
+                <div className="accordion-item mb-3">
+                  <div className="accordion-header font-base" id="heading-1">
+                    <button className="accordion-button fw-semibold rounded collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="true" aria-controls="collapse-1">
+                      How many items are there on this list?
+                    </button>
+                  </div>
+                  <div id="collapse-1" className="accordion-collapse collapse show" aria-labelledby="heading-1" data-bs-parent="#accordionFaq">
+                    <div className="accordion-body mt-3 pb-0">
+                      Was out laughter raptures returned outweigh. Luckily cheered colonel I do we attack highest enabled. Tried law yet style child. The bore of true of no be deal. Drawings offended yet answered Jennings perceive laughing six did far. Rooms oh fully taken by worse do. Points afraid but may end law lasted.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="accordion-item mb-3">
+                  <div className="accordion-header font-base" id="heading-2">
+                    <button className="accordion-button fw-semibold rounded collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="false" aria-controls="collapse-2">
+                      Can you describe mizzle in just one sentence?
+                    </button>
+                  </div>
+                  <div id="collapse-2" className="accordion-collapse collapse" aria-labelledby="heading-2" data-bs-parent="#accordionFaq">
+                    <div className="accordion-body mt-3">
+                      September how men saw tolerably two behavior arranging. She offices for highest and replied one venture pasture. Applauded no discovery in newspaper allowance am northward. Frequently partiality possession resolution at or appearance unaffected me. Engaged its was the evident pleased husband. Ye goodness felicity do disposal dwelling no. First am plate jokes to began to cause a scale. Subjects he prospect elegance followed no overcame possible it on. Improved own provided blessing may peculiar domestic. Sight house has sex never. No visited raising gravity outward subject my cottage Mr be.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="accordion-item mb-3">
+                  <div className="accordion-header font-base" id="heading-3">
+                    <button className="accordion-button fw-semibold collapsed rounded" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-3" aria-expanded="false" aria-controls="collapse-3">
+                      What services do agencies provide?
+                    </button>
+                  </div>
+                  <div id="collapse-3" className="accordion-collapse collapse" aria-labelledby="heading-3" data-bs-parent="#accordionFaq">
+                    <div className="accordion-body mt-3">
+                      Agencies provide a wide range of services depending on their specialization. Some common services include advertising campaigns, digital marketing, branding, creative design, media planning and buying, public relations, talent management, event planning, and market research.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="accordion-item mb-3">
+                  <div className="accordion-header font-base" id="heading-4">
+                    <button className="accordion-button fw-semibold collapsed rounded" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-4" aria-expanded="false" aria-controls="collapse-4">
+                      How do I choose the right agency for my needs?
+                    </button>
+                  </div>
+                  <div id="collapse-4" className="accordion-collapse collapse" aria-labelledby="heading-4" data-bs-parent="#accordionFaq">
+                    <div className="accordion-body mt-3">
+                      When selecting an agency, consider your specific requirements, budget, and the agency's expertise and track record in your industry. Research their portfolio, client testimonials, and case studies to gauge their capabilities. It's also important to meet with the agency to assess their communication style and ensure they align with your goals.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="accordion-item mb-3">
+                  <div className="accordion-header font-base" id="heading-5">
+                    <button className="accordion-button fw-semibold collapsed rounded" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-5" aria-expanded="false" aria-controls="collapse-5">
+                      What are the advantages of hiring an agency?
+                    </button>
+                  </div>
+                  <div id="collapse-5" className="accordion-collapse collapse" aria-labelledby="heading-5" data-bs-parent="#accordionFaq">
+                    <div className="accordion-body mt-3">
+                      Hiring an agency offers several benefits, such as access to specialized skills and expertise, industry insights, creative thinking, established networks, and resources. Agencies can provide fresh perspectives, save time and effort, and help you achieve your objectives more effectively.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="accordion-item mb-3">
+                  <div className="accordion-header font-base" id="heading-6">
+                    <button className="accordion-button fw-semibold collapsed rounded" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-6" aria-expanded="false" aria-controls="collapse-6">
+                      How long does it take for an agency to deliver results?
+                    </button>
+                  </div>
+                  <div id="collapse-6" className="accordion-collapse collapse" aria-labelledby="heading-6" data-bs-parent="#accordionFaq">
+                    <div className="accordion-body mt-3">
+                      The timeframe for achieving results depends on the nature of the project and its complexity. Short-term campaigns may yield results within weeks, while long-term strategies may take several months or even years to show significant impact. Effective collaboration, clear goals, and realistic expectations can help expedite the process.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="accordion-item mb-3">
+                  <div className="accordion-header font-base" id="heading-7">
+                    <button className="accordion-button fw-semibold collapsed rounded" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-7" aria-expanded="false" aria-controls="collapse-7">
+                      How can I measure the success of agency campaigns or services?
+                    </button>
+                  </div>
+                  <div id="collapse-7" className="accordion-collapse collapse" aria-labelledby="heading-7" data-bs-parent="#accordionFaq">
+                    <div className="accordion-body mt-3">
+                      Success metrics depend on the goals and objectives of your project. Common metrics include increased sales or revenue, improved brand recognition, website traffic growth, social media engagement, lead generation, customer acquisition, or positive media coverage. Work with the agency to define key performance indicators (KPIs) and establish measurement mechanisms.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="accordion-item mb-3">
+                  <div className="accordion-header font-base" id="heading-8">
+                    <button className="accordion-button fw-semibold collapsed rounded" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-8" aria-expanded="false" aria-controls="collapse-8">
+                      Can I terminate my contract with an agency if I'm not satisfied?
+                    </button>
+                  </div>
+                  <div id="collapse-8" className="accordion-collapse collapse" aria-labelledby="heading-8" data-bs-parent="#accordionFaq">
+                    <div className="accordion-body mt-3">
+                      Yes, you can terminate your contract with an agency if you're not satisfied, although the specific terms of termination should be outlined in the contract. It's advisable to communicate any concerns or issues with the agency first to see if they can address them. If the relationship is not salvageable, you may need to follow the agreed-upon termination procedures.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="get-started" className="pt-0">
         <div className="container">
 
           <div className="inner-container-small mx-auto">
